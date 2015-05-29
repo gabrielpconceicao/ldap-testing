81de463ffd97e2dafa621a0b12ca162eb0a943d1
Gabriel Conceição <gabriel_7340@hotmail.com>
2015-05-29 18:58:35 +0100

    up server client auth changes

--- ldap-client-auth.js
+++ ldap-client-auth.js
@@ -4,8 +4,10 @@ var client = ldap.createClient({
 	url: 'ldap://127.0.0.1:1389'
 });
 
-client.bind( 'cn=foo', 'secret', function( err ) {
+client.bind( 'cn=gabriel_7340@hotmail.com', 'password', function( err, res ) {
 	if( err ){
 		console.log( 'bind error: ', err );
+	} else {
+		console.log( 'login ok' );
 	}
 });
--- ldap-server-auth.js
+++ ldap-server-auth.js
@@ -1,23 +1,52 @@
 var ldap = require( 'ldapjs' ),
+    mysql = require('mysql'),
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'database',
+    }),
+    tablename: 'users',
     server = ldap.createServer();
 
-var validUser = {
-    name: 'foo',
-    pwd: 'secret',
-};
+function authenticate( email, pass, cb ){
+    connection.connect();
+    var found = false;
+
+    connection.query( 'select * from ' + tablename, function( err, rows, fields ) {
+        if ( err ) throw err;
+
+        for( var i = 0; i < rows.length; i++ ){
+            if( rows[ i ].email == email && rows[ i ].pass == pass ){
+                found = true;
+                cb( { id: rows[ i ].id } );
+            }
+        }
+
+        if( !found ){
+             cb( { error: new ldap.InvalidCredentialsError() } );
+        }
+    });
+
+    connection.end();
+}
 
 server.listen( 1389, function() {
     console.log( 'server up at: %s', server.url );
 
-    server.bind( 'cn=foo', function( req, res, next ) {
+    server.bind( 'cn=gabriel_7340@hotmail.com', function( req, res, next ) {
+
         var user = req.dn.toString().replace( 'cn=', '' ),
-        pass = req.credentials;
+            pass = req.credentials;
+           
+        authenticate( user, pass, function( login ){
+            if( login.id ){
+                res.end();
+            } else {
+                return next( login.error );
+            }
+          
+        });
 
-        if( validUser.name != user || validUser.pwd != pass ) {
-          return next(new ldap.InvalidCredentialsError());
-        }
-
-        res.end();
-        return next( 'valid' );
     });
 });
